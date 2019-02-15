ca3fa38a126dad9018d4963a5f5c1e3180a4b2b8
John Player <johnplayer1982@msn.com>
2019-02-15 11:06:40 +0000

    Adds length example

---
+++ ex_length.py
@@ -0,0 +1,15 @@
+# Get the length
+
+# Create a list with 5 objects
+list = [
+    23,
+    43,
+    "dog",
+    34.0,
+    "text"
+]
+
+# Print the length of the list
+# Output = 5
+listlength = len(list)
+print listlength
