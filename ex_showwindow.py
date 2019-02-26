e406727e2bbbb9637618473035cae2d3c5e64de1
John Player <johnplayer1982@msn.com>
2019-02-26 08:08:09 +0000

    Adds more details to the show window example

--- ex_showwindow.py
+++ ex_showwindow.py
@@ -2,26 +2,75 @@
 # ---------------------------
 # import ex_showwindow as sw
 # reload(sw)
+# sw.showWindow().buildUI()
 # ---------------------------
 
 # Import the Maya commands library
 from maya import cmds
 
-# Create a window using the cmds.window command
-# give it a title, icon and dimensions
-window = cmds.window( title="Long Name", iconName='Short Name', widthHeight=(200, 55) )
+class showWindow(object):
 
-# As we add contents to the window, align them vertically
-cmds.columnLayout( adjustableColumn=True )
+    windowName = 'WindowName'
 
-# A button that does nothing
-cmds.button( label='Do Nothing' )
+    def __init__(self):
 
-# Close button with a command to delete the UI
-cmds.button( label='Close', command=('cmds.deleteUI(\"' + window + '\", window=True)') )
+        print 'Initialized'
 
-# Set its parent to the Maya window (denoted by '..')
-cmds.setParent( '..' )
+    def buildUI(self):
 
-# Show the window that we created (window)
-cmds.showWindow( window )
+        # Create a window using the cmds.window command
+        # give it a title, icon and dimensions
+
+        window = cmds.window( title="Long Name", iconName='Short Name', widthHeight=(300, 300) )
+
+        # As we add contents to the window, align them vertically
+        cmds.columnLayout(adjustableColumn=True)
+
+        # Add some intro text
+        cmds.text(label="Some intro text here")
+
+        # For column Layout use:
+        # cmds.columnLayout()
+        # And define the column numbers using:
+        # cmds.rowLayout(numberOfColumns=4)
+
+        # Add a number slider (integer)
+        # Set min and max values
+        # value = default value
+        # step = increment
+        # dragCommand = function to run on each step
+
+        self.slider = cmds.intSlider(min=2, max=30, value=5, step=1, dragCommand=self.updateLabel)
+
+        # Label for the slider
+        # We want to update this when the slider is moved
+
+        self.label = cmds.text(label="5")
+
+        # A button that does nothing
+
+        cmds.button(label='Do Nothing')
+
+        # Close button with a command to delete the UI
+
+        cmds.button(label='Close', command=('cmds.deleteUI(\"' + window + '\", window=True)'))
+
+        # Set its parent to the Maya window (denoted by '..')
+
+        cmds.setParent( '..' )
+
+        # Show the window that we created (window)
+
+        cmds.showWindow( window )
+
+    # The value is passed from the dragCommand attribute on line 43:
+
+    def updateLabel(self, value):
+
+        # Update the label on the slider
+
+        cmds.text(self.label, edit=True, label=value)
+        print value
+
+
+
